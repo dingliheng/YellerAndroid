e12605c3ef1e0dfb367c1dfdff165783a3c7e221
hidemon <yusun@utexas.edu>
2015-11-07 20:01:12 -0600

    Amend

---
+++ app/src/main/java/edu/utaustin/yusun/yellerandroid/PublishMoodActivity.java
@@ -0,0 +1,13 @@
+package edu.utaustin.yusun.yellerandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PublishMoodActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_publish_mood);
+    }
+}
--- app/src/main/java/edu/utaustin/yusun/yellerandroid/UploadImageActivity.java
+++ app/src/main/java/edu/utaustin/yusun/yellerandroid/UploadImageActivity.java
@@ -1,9 +1,13 @@
 package edu.utaustin.yusun.yellerandroid;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.ImageView;
 
 public class UploadImageActivity extends Activity {
@@ -12,9 +16,40 @@ public class UploadImageActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_image);
+        // Set up the action bar.
+        final ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
         Intent intent = getIntent();
         bitmap = (Bitmap) intent.getParcelableExtra(LaunchpadSectionFragment.BITMAPIMAGE);
         ImageView imageView = (ImageView) findViewById(R.id.imageView);
         imageView.setImageBitmap(bitmap);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // app icon in action bar clicked; go home
+                final CharSequence[] items = {"Yes!", "Opps..Stay here"};
+                AlertDialog.Builder builder = new AlertDialog.Builder(UploadImageActivity.this);
+                builder.setTitle("Are you sure to give up the upload?");
+                builder.setItems(items, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int item) {
+                        if (items[item].equals("Yes!")) {
+                            Intent intent = new Intent(UploadImageActivity.this, MainActivity.class);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                            startActivity(intent);
+                        } else if (items[item].equals("Opps..Stay here")) {
+                            dialog.dismiss();
+                        }
+                    }
+                });
+                builder.show();
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
