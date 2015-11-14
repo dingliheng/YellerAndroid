f7a9a8042927c97c6853960010cf4c9364297229
hidemon <yusun@utexas.edu>
2015-11-13 20:58:28 -0600

    ListView

--- app/src/main/java/edu/utaustin/yusun/yellerandroid/LaunchpadSectionFragment.java
+++ app/src/main/java/edu/utaustin/yusun/yellerandroid/LaunchpadSectionFragment.java
@@ -19,8 +19,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.beardedhen.androidbootstrap.BootstrapButton;
 
@@ -29,6 +32,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * A fragment that launches other parts of the demo application.
@@ -38,6 +42,13 @@ public class LaunchpadSectionFragment extends Fragment {
     Button btnSelect;
     ImageView ivImage;
     public static final String BITMAPIMAGE = "BitmapImage";
+
+    private PullToRefreshListView listView;
+    private PullToRefreshListViewSampleAdapter adapter;
+
+    // IDs for the context menu actions
+    private final int idEdit = 1;
+    private final int idDelete = 2;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -89,6 +100,77 @@ public class LaunchpadSectionFragment extends Fragment {
             }
         });
 
+        listView = (PullToRefreshListView) rootView.findViewById(R.id.pull_to_refresh_listview);
+
+        // OPTIONAL: Disable scrolling when list is refreshing
+        // listView.setLockScrollWhileRefreshing(false);
+
+        // OPTIONAL: Uncomment this if you want the Pull to Refresh header to show the 'last updated' time
+        // listView.setShowLastUpdatedText(true);
+
+        // OPTIONAL: Uncomment this if you want to override the date/time format of the 'last updated' field
+        // listView.setLastUpdatedDateFormat(new SimpleDateFormat("yyyy/MM/dd HH:mm:ss"));
+
+        // OPTIONAL: Uncomment this if you want to override the default strings
+        // listView.setTextPullToRefresh("Pull to Refresh");
+        // listView.setTextReleaseToRefresh("Release to Refresh");
+        // listView.setTextRefreshing("Refreshing");
+
+        // MANDATORY: Set the onRefreshListener on the list. You could also use
+        // listView.setOnRefreshListener(this); and let this Activity
+        // implement OnRefreshListener.
+        listView.setOnRefreshListener(new PullToRefreshListView.OnRefreshListener() {
+
+            @Override
+            public void onRefresh() {
+                // Your code to refresh the list contents goes here
+
+                // for example:
+                // If this is a webservice call, it might be asynchronous so
+                // you would have to call listView.onRefreshComplete(); when
+                // the webservice returns the data
+                adapter.loadData();
+
+                // Make sure you call listView.onRefreshComplete()
+                // when the loading is done. This can be done from here or any
+                // other place, like on a broadcast receive from your loading
+                // service or the onPostExecute of your AsyncTask.
+
+                // For the sake of this sample, the code will pause here to
+                // force a delay when invoking the refresh
+                listView.postDelayed(new Runnable() {
+
+
+                    @Override
+                    public void run() {
+                        listView.onRefreshComplete();
+                    }
+                }, 2000);
+            }
+        });
+
+        adapter = new PullToRefreshListViewSampleAdapter() {};
+        listView.setAdapter(adapter);
+
+        // Request the adapter to load the data
+        adapter.loadData();
+
+        // click listener
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+                                    long arg3) {
+
+                PullToRefreshListViewSampleAdapter.ViewHolder viewHolder = (PullToRefreshListViewSampleAdapter.ViewHolder) arg1.getTag();
+                if (viewHolder.name != null){
+                    Toast.makeText(getContext(), viewHolder.name.getText(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        // Register the context menu for actions
+        registerForContextMenu(listView);
         return rootView;
     }
 
@@ -115,6 +197,8 @@ public class LaunchpadSectionFragment extends Fragment {
                 }
             }
         });
+
+
         builder.show();
     }
 
@@ -183,6 +267,94 @@ public class LaunchpadSectionFragment extends Fragment {
         intent.putExtra(BITMAPIMAGE, bm);
         startActivity(intent);
 
+    }
+
+    /**
+     * The adapter used to display the results in the list
+     *
+     */
+    public abstract class PullToRefreshListViewSampleAdapter extends android.widget.BaseAdapter {
+
+        private ArrayList<String> items = new ArrayList<String>();;
+
+        public class ViewHolder {
+            public String id;
+            public TextView name;
+        }
+
+        /**
+         * Loads the data.
+         */
+        public void loadData() {
+
+            // Here add your code to load the data for example from a webservice or DB
+
+            items = new ArrayList<String>();
+
+            items.add("Ajax Amsterdam");
+            items.add("Barcelona");
+            items.add("Manchester United");
+            items.add("Chelsea");
+            items.add("Real Madrid");
+            items.add("Bayern Munchen");
+            items.add("Internazionale");
+            items.add("Valencia");
+            items.add("Arsenal");
+            items.add("AS Roma");
+            items.add("Tottenham Hotspur");
+            items.add("PSV");
+            items.add("Olympique Lyon");
+            items.add("AC Milan");
+            items.add("Dortmund");
+            items.add("Schalke 04");
+            items.add("Twente");
+            items.add("Porto");
+            items.add("Juventus");
+
+
+            // MANDATORY: Notify that the data has changed
+            notifyDataSetChanged();
+        }
+
+        @Override
+        public int getCount() {
+            return items.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return items.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View rowView = convertView;
+
+            String record = (String) getItem(position);
+
+            LayoutInflater inflater = getActivity().getLayoutInflater();
+
+            ViewHolder viewHolder = new ViewHolder();
+
+            if (convertView == null){
+                rowView = inflater.inflate(R.layout.list_item,null);
+
+                viewHolder.name = (TextView) rowView.findViewById(R.id.textView1);
+
+                rowView.setTag(viewHolder);
+            }
+
+            final ViewHolder holder = (ViewHolder) rowView.getTag();
+
+            holder.name.setText(record);
+
+            return rowView;
+        }
     }
 }
 
