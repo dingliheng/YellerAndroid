46e2b0c5548d2b8204cbd97396ba6789bc1f3dc5
hidemon <yusun@utexas.edu>
2015-12-02 20:48:24 -0600

    Searchfriend xml

---
+++ app/src/main/java/edu/utaustin/yusun/yellerandroid/friends_activities/SearchFriendActivity.java
@@ -0,0 +1,15 @@
+package edu.utaustin.yusun.yellerandroid.friends_activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import edu.utaustin.yusun.yellerandroid.R;
+
+public class SearchFriendActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_friend);
+    }
+}
